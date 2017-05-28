2a2aa7c165842ae3a280154bd9ae23d2c1e8d2b0
Kaya Winn <mail@psyreal.net>
2017-05-28 13:51:47 -0700

    updates layout of notes and search

--- app/src/main/java/com/example/kaya/connectus2/SearchActivity.java
+++ app/src/main/java/com/example/kaya/connectus2/SearchActivity.java
@@ -1,13 +1,27 @@
 package com.example.kaya.connectus2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SearchActivity extends AppCompatActivity {
+
+private ImageButton mFindDoctorButton;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_search);
+    mFindDoctorButton = (ImageButton) findViewById(R.id.findDoctorButton);
+    mFindDoctorButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(SearchActivity.this, FindDoctorActivity.class);
+            startActivity(intent);
+        }
+    });
+
 }
 }
