a1ace1a75ed4bf24845335cbde072f41889df86e
thoreax@yahoo.com <mail@psyreal.net>
2017-05-28 18:54:33 -0700

    adds validation on Notes field at save

--- app/src/main/java/com/example/kaya/connectus2/SearchActivity.java
+++ app/src/main/java/com/example/kaya/connectus2/SearchActivity.java
@@ -5,13 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageButton;
+
 import android.widget.ListView;
 
 public class SearchActivity extends AppCompatActivity {
-    private ImageButton mFindDoctorButton;
-    private EditText mSymptomEditText;
+
     private ListView mListView;
     private String[] symptoms = new String[]{"bleeding","bruising","chills and shivering","convulsions","deformity","discharge","dizziness","dry mouth ","epistaxis","fatigue","hyperthermia","hypothermia","jaundice", "loss of appetite","muscle cramps","muscle weakness","pain - abdominal","pain - chest ","pyrexia","swelling","syncope","tinnitus","tremor","weight gain","weight loss" };
 
@@ -20,15 +18,13 @@ protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_search);
 
-    mFindDoctorButton = (ImageButton) findViewById(R.id.findDoctorButton);
+
     mListView = (ListView) findViewById(R.id.symptomsListView);
 
 
 
     ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, symptoms);
     mListView.setAdapter(adapter);
-
-
 
 
 }
