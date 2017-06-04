48bd21872c71a1941811163b331a75f3eb6c66ae
thoreax@yahoo.com <mail@psyreal.net>
2017-06-04 11:52:16 -0700

    converts symptom listview to custom symptom listview

--- app/src/main/java/com/example/kaya/connectus2/ui/SymptomListActivity.java
+++ app/src/main/java/com/example/kaya/connectus2/ui/SymptomListActivity.java
@@ -1,31 +1,44 @@
 package com.example.kaya.connectus2.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.kaya.connectus2.R;
+import com.example.kaya.connectus2.adapters.SymptomListAdapter;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SymptomListActivity extends AppCompatActivity {
 
-    private ListView mListView;
+@Bind(R.id.symptomsListView) ListView mListView;
+
     private String[] symptoms = new String[]{"bleeding","bruising","chills and shivering","convulsions","deformity","discharge","dizziness","dry mouth ","epistaxis","fatigue","hyperthermia","hypothermia","jaundice", "loss of appetite","muscle cramps","muscle weakness","pain - abdominal","pain - chest ","pyrexia","swelling","syncope","tinnitus","tremor","weight gain","weight loss" };
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_symptomList);
+    setContentView(R.layout.activity_symptom_list);
+    ButterKnife.bind(this);
 
-
-    mListView = (ListView) findViewById(R.id.symptomsListView);
-
-
-
-    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, symptoms);
+    SymptomListAdapter adapter = new SymptomListAdapter(this, android.R.layout.simple_list_item_1, symptoms);
     mListView.setAdapter(adapter);
 
+    mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String symptom = ((TextView) view).getText().toString();
+            Intent intent = new Intent(SymptomListActivity.this, DoctorListActivity.class);
+            intent.putExtra("params", symptom);
+            intent.putExtra("parent", "symptom");
+            startActivity(intent);
+        }
+    });
 
 }
 }
