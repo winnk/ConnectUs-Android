06ae32418bc31cc325dfd5a7a134c17da64a3993
thoreax@yahoo.com <mail@psyreal.net>
2017-06-04 12:32:22 -0700

    update doctor activity list to accept intent

--- app/src/main/java/com/example/kaya/connectus2/ui/DoctorListActivity.java
+++ app/src/main/java/com/example/kaya/connectus2/ui/DoctorListActivity.java
@@ -4,21 +4,73 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.kaya.connectus2.R;
+import com.example.kaya.connectus2.adapters.DoctorListAdapter;
+import com.example.kaya.connectus2.model.Doctor;
+import com.example.kaya.connectus2.services.BDService;
+import java.io.IOException;
+import java.util.ArrayList;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class DoctorListActivity extends AppCompatActivity {
-    private TextView mSymptomTextView;
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_find_doctor);
-    //mSymptomTextView = (TextView) findViewById(R.id.symptomTextView);
+    @Bind(R.id.textView6) TextView mTextView;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private DoctorListAdapter mAdapter;
 
-    Intent intent = getIntent();
-    String symptom = intent.getStringExtra("symptom");
-    mSymptomTextView.setText("Here are all the physicians who treat your symptom: " + symptom);
+    public ArrayList<Doctor> mDoctors = new ArrayList<>();
 
+    private String parentString;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_doctor);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String searchSymptom = intent.getStringExtra("params");
+        parentString = intent.getStringExtra("parent");
+
+        mTextView.setText("Results for \"" + searchSymptom + "\"");
+        getDoctors(searchSymptom);
+    }
+
+    private void getDoctors(String searchSymptom){
+
+        final BDService bdService = new BDService();
+        BDService.findBySymptom(searchSymptom, new Callback(){
+
+            @Override
+            public void onFailure(Call call, IOException e) { e.printStackTrace(); }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mDoctors = bdService.processResults(response);
+
+                DoctorListActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mAdapter = new DoctorListAdapter(getApplicationContext(), mDoctors);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(DoctorListActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+
+                    ;
+                });
+            }
+        });
+    }
 }
-}
+
