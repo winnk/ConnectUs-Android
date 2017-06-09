5bd03e044cc9a145459d2a1f8d010e2b40a7006e
thoreax@yahoo.com <mail@psyreal.net>
2017-06-09 10:57:16 -0700

    adds note input validation

--- app/src/main/java/com/example/kaya/connectus2/ui/NotesActivity.java
+++ app/src/main/java/com/example/kaya/connectus2/ui/NotesActivity.java
@@ -7,8 +7,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.kaya.connectus2.Constants;
+import com.example.kaya.connectus2.Helpers.InputValidatorHelper;
 import com.example.kaya.connectus2.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -27,6 +29,7 @@ private DatabaseReference mFirebaseNoteBody;
 Button mSaveNoteButton;
 @Bind(R.id.noteBodyET)
 EditText mNoteBodyET;
+public String mNote;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +51,32 @@ public void onClick(View v) {
 
 
     if (v == mSaveNoteButton) {
+        String mNote = mNoteBodyET.getText().toString();
 
-        String note = mNoteBodyET.getText().toString();
-        boolean validNote = isValidNote(note);
-        saveNoteToFirebase(note);
+        InputValidatorHelper inputValidatorHelper = new InputValidatorHelper();
+        StringBuilder errMsg = new StringBuilder("Unable to save. Please fix the following and try again\n");
 
-        Intent intent = new Intent(NotesActivity.this, NotesListActivity.class);
-        Log.d("notes save", note);
-        startActivity(intent);
+//Validate and Save
+        boolean allowSave = true;
+        //calls validator helper class
+        if (inputValidatorHelper.isNullOrEmpty(mNote)) {
+            errMsg.append("- Note should not be empty.\n");
+            Toast.makeText(NotesActivity.this, errMsg,
+                    Toast.LENGTH_SHORT).show();
+            allowSave = false;
+        }
+
+        if(allowSave){
+            //Proceeds with save logic
+
+            String note = mNoteBodyET.getText().toString();
+
+            saveNoteToFirebase(note);
+
+            Intent intent = new Intent(NotesActivity.this, NotesListActivity.class);
+            Log.d("notes save", note);
+            startActivity(intent);
+        }
     }
 }
 
@@ -63,13 +84,6 @@ public void saveNoteToFirebase(String note) {
     mFirebaseNoteBody.push().setValue(note);
     }
 
-private boolean isValidNote(String note) {
-    if (note.equals("")) {
-        mNoteBodyET.setError("Please enter a note");
-        return false;
-    }
-    return true;
-}
 
 }
 
