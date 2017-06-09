5f6cc0d764ed277c36861d1bd780dd455d31ec6e
thoreax@yahoo.com <mail@psyreal.net>
2017-06-09 13:38:59 -0700

    adds validation to note upon save of note object for !null

---
+++ app/src/main/java/com/example/kaya/connectus2/model/Note.java
@@ -0,0 +1,65 @@
+package com.example.kaya.connectus2.model;
+
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.view.View;
+
+
+import java.util.Date;
+
+import static android.R.attr.format;
+
+
+/**
+ * TODO: Add a class header comment!
+ */
+
+public class Note {
+String date;
+String body;
+String title;
+private String pushId;
+
+public Note(){}
+
+public Note (String noteBody, String noteTitle){
+    this.body = noteBody;
+    this.title = noteTitle;
+
+    java.util.Calendar cal = java.util.Calendar.getInstance();
+    String year = cal.get(java.util.Calendar.YEAR) + "";
+    String month = (cal.get(java.util.Calendar.MONTH) + 1) + "";
+    String day = cal.get(java.util.Calendar.DAY_OF_MONTH) + "";
+    Log.d("MainActivity", "Current Timestamp: " + format);
+    this.date = month + " / " +day+ " / " + year;
+
+
+}
+
+public String getNote() {
+    return body;
+}
+
+public String getTitle(){
+    return title;
+}
+
+public String getDate() {
+    return date;
+}
+
+public String getPushId(){
+    return pushId;
+}// end getters
+
+public void setPushId(String pushId) {
+    this.pushId = pushId;
+}
+
+
+}
+
+
--- app/src/main/java/com/example/kaya/connectus2/ui/NotesActivity.java
+++ app/src/main/java/com/example/kaya/connectus2/ui/NotesActivity.java
@@ -12,11 +12,16 @@ import android.widget.Toast;
 import com.example.kaya.connectus2.Constants;
 import com.example.kaya.connectus2.Helpers.InputValidatorHelper;
 import com.example.kaya.connectus2.R;
+import com.example.kaya.connectus2.model.Note;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
+import static java.security.AccessController.getContext;
 
 
 public class NotesActivity extends AppCompatActivity implements View.OnClickListener {
@@ -25,11 +30,10 @@ public class NotesActivity extends AppCompatActivity implements View.OnClickList
 
 private DatabaseReference mFirebaseNoteBody;
 
-@Bind(R.id.saveNoteButton)
-Button mSaveNoteButton;
-@Bind(R.id.noteBodyET)
-EditText mNoteBodyET;
-public String mNote;
+@Bind(R.id.saveNoteButton)Button mSaveNoteButton;
+@Bind(R.id.noteBodyET)EditText mNoteBodyET;
+@Bind(R.id.noteTitleET) EditText mNoteTitle;
+
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +41,7 @@ protected void onCreate(Bundle savedInstanceState) {
     mFirebaseNoteBody = FirebaseDatabase
             .getInstance()
             .getReference()
-            .child(Constants.BD_NOTE_BODY);
+            .child(Constants.FB_NOTE_BODY);
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_notes);
@@ -49,18 +53,20 @@ protected void onCreate(Bundle savedInstanceState) {
 @Override
 public void onClick(View v) {
 
-
     if (v == mSaveNoteButton) {
-        String mNote = mNoteBodyET.getText().toString();
+        String noteBody = mNoteBodyET.getText().toString();
+        String noteTitle = mNoteTitle.getText().toString();
 
+// Calls validator
         InputValidatorHelper inputValidatorHelper = new InputValidatorHelper();
-        StringBuilder errMsg = new StringBuilder("Unable to save. Please fix the following and try again\n");
+        StringBuilder errMsg = new StringBuilder("Unable to save. ");
 
 //Validate and Save
         boolean allowSave = true;
-        //calls validator helper class
-        if (inputValidatorHelper.isNullOrEmpty(mNote)) {
-            errMsg.append("- Note should not be empty.\n");
+
+//calls validator helper class
+        if (inputValidatorHelper.isNullOrEmpty(noteTitle) || inputValidatorHelper.isNullOrEmpty(noteBody)) {
+            errMsg.append("- Title and note should not be empty.\n");
             Toast.makeText(NotesActivity.this, errMsg,
                     Toast.LENGTH_SHORT).show();
             allowSave = false;
@@ -69,22 +75,34 @@ public void onClick(View v) {
         if(allowSave){
             //Proceeds with save logic
 
-            String note = mNoteBodyET.getText().toString();
+            Note note = new Note(noteBody, noteTitle);
 
-            saveNoteToFirebase(note);
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            String uid = user.getUid();
+
+            DatabaseReference noteRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FB_CHILD_NOTES)
+                    .child(uid);
+
+            DatabaseReference pushRef = noteRef.push();
+            String pushId = pushRef.getKey();
+            note.setPushId(pushId);
+            pushRef.setValue(note);
+
+            Toast.makeText(NotesActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+            ResetFields();
 
             Intent intent = new Intent(NotesActivity.this, NotesListActivity.class);
-            Log.d("notes save", note);
+            Log.d("notes save", noteBody);
             startActivity(intent);
         }
     }
 }
-
-public void saveNoteToFirebase(String note) {
-    mFirebaseNoteBody.push().setValue(note);
-    }
-
-
+public void ResetFields() {
+    mNoteBodyET.setText("");
+    mNoteTitle.setText("");
+}
 }
 
 
